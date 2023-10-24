170550c986dbba256ba25b89cb95a1f68e22038d
Nilinz <greyhort@gmail.com>
2023-10-24 22:43:24 +0300

    first

---
+++ quotes_app/quotes/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import TagForm
+
+def main(request):
+    return render(request, 'quotes/index.html')
+
+def tag(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:main')
+        else:
+            return render(request, 'quotes/tag.html', {'form': form})
+
+    return render(request, 'quotes/tag.html', {'form': TagForm()})
