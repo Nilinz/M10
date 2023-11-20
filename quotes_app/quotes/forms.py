0ee8e426226c6d26887d085a4ff43b231924ac46
Nilinz <greyhort@gmail.com>
2023-11-20 23:07:43 +0200

    update

--- quotes_app/quotes/forms.py
+++ quotes_app/quotes/forms.py
@@ -14,39 +14,3 @@ class QuoteForm(forms.ModelForm):
     class Meta:
         model = Quote
         fields = ['quote']
-# class QuoteForm(forms.ModelForm):
-# #     author_name = forms.CharField(max_length=255, required=True, label='Author')
-
-#     class Meta:
-#         model = Quote
-#         fields = ['quote', 'tags', 'author']  
-
-# class QuoteForm(forms.ModelForm):
-#     # Поле для введення тексту тегів
-#     tags = forms.CharField(max_length=255, required=False, help_text="Enter tags separated by commas")
-
-#     # Поле для введення імені автора
-#     author_name = forms.CharField(max_length=255, required=True, label='Author')
-
-#     class Meta:
-#         model = Quote
-#         fields = ['quote', 'tags', 'author_name']
-
-#     def clean_tags(self):
-#         # Отримати значення поля тегів
-#         tags = self.cleaned_data['tags']
-#         # Розділити теги за комою і видалити пробіли
-#         tag_list = [tag.strip() for tag in tags.split(',') if tag.strip()]
-#         # Повернути список тегів
-#         return tag_list
-
-#     def clean_author_name(self):
-#         # Перевірити, чи існує автор з таким ім'ям у базі даних
-#         author_name = self.cleaned_data['author_name']
-#         try:
-#             author = Author.objects.get(fullname=author_name)
-#         except Author.DoesNotExist:
-#             # Якщо автор не існує, створити його
-#             author = Author.objects.create(fullname=author_name)
-#         # Повернути об'єкт автора
-#         return author
--- quotes_app/quotes_app/settings.py
+++ quotes_app/quotes_app/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j2fe4zg&yz+&(b#xzj(6u7*&xjsxcpf5gmdrn%p1c@%5t6-93s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,11 +82,11 @@ WSGI_APPLICATION = 'quotes_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '567234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
@@ -126,4 +130,16 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_STARTTLS = False
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
